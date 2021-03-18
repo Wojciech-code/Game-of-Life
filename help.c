a27d6abc243f81b903aceb4d48cf4e0abb502e86
wk <wojtekkudeko@gmail.com>
2021-03-18 10:45:48 +0100

    Kosmetyka

--- help.c
+++ help.c
@@ -4,11 +4,11 @@
 
 
 void help_(char *progname){
-	char *usage = " Usage: ./gof -d dane/nazwa -i ilość iteracji -g nazwa pliku dla ostatniej iteracji -z zmienia zasady gry na neumanna -f tworzy gifa\n\n";
+	char *usage = " Usage: ./gof -d dane/nazwa -i liczba iteracji -g nazwa pliku dla ostatniej generacji -z zmienia sąsiedztwo na neumanna -f tworzy gifa\n\n";
 	char *opis = "Jeśli plik z danymi nie jest podany program nie zacznie pracy\n"
-		"Jeśli ilość iteracji nie będzie podana program domyśnie przyjmie 5\n"
+		"Jeśli liczba iteracji nie będzie podana program domyśnie przyjmie 5\n"
 		"Ostatnia generacja zostanie stworzona do pliku jeśli zostanie podana nazwa tego pliku\n"
-		"Dodanie flagi -z spowoduje zmienienie zasad gry na sąsiedztwo neumanna ( domyślnie jest to sąsiedztwo moora )\n";
+		"Dodanie flagi -z spowoduje zmienienie sąsiedztwa moora na sąsiedztwo neumanna \n";
 
 	fprintf(stderr, usage, progname);
 	fprintf(stderr, opis, progname);
